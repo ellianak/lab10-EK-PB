10ac052d17a51d88ad638aa70cfdbfdcf94e0287
elliana :) <elliana.kirby@ufl.edu>
2025-04-09 15:42:22 -0400

    Finished test_calculator ???

--- test_calculator.py
+++ test_calculator.py
@@ -15,11 +15,15 @@ class TestCalculator(unittest.TestCase):
     # ##########################
 
     ######## Partner 1
-    # def test_multiply(self): # 3 assertions
-    #     fill in code
+    def test_multiply(self): # 3 assertions
+        self.assertTrue(mul(2, 2))
+        self.assertTrue(mul(-3, 4))
+        self.assertTrue(mul(-5, -7))
 
-    # def test_divide(self): # 3 assertions
-    #     fill in code
+    def test_divide(self): # 3 assertions
+        self.assertTrue(div(2, 3))
+        self.assertTrue(div(-4, -5))
+        self.assertTrue(div(5, -4))
     # ##########################
 
     ######## Partner 2
@@ -38,21 +42,26 @@ class TestCalculator(unittest.TestCase):
         self.assertRaises(ValueError, log(-5,125))
     
     ######## Partner 1
-    # def test_log_invalid_argument(self): # 1 assertion
-    #     # call log function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     logarithm(0, 5)
-    #     fill in code
+    def test_log_invalid_argument(self): # 1 assertion
+        # call log function inside, example:
+        # with self.assertRaises(<INSERT_ERROR_TYPE>):
+        #     logarithm(0, 5)
+        self.assertRaises(ValueError, log(4, 0))
 
-    # def test_hypotenuse(self): # 3 assertions
-    #     fill in code
+    def test_hypotenuse(self): # 3 assertions
+        self.assertTrue(hypotenuse(2, 2))
+        self.assertTrue(hypotenuse(-3, 4))
+        self.assertTrue(hypotenuse(-5, -7))
 
-    # def test_sqrt(self): # 3 assertions
-    #     # Test for invalid argument, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #    square_root(NUM)
-    #     # Test basic function
-    #     fill in code
+    def test_sqrt(self): # 3 assertions
+        # Test for invalid argument, example:
+        # with self.assertRaises(<INSERT_ERROR_TYPE>):
+        #    square_root(NUM)
+        self.assertRaises(ValueError, square_root(-4))
+        # Test basic function
+        self.assertTrue(square_root(2))
+        self.assertTrue(square_root(4))
+        self.assertTrue(square_root(99))
     ##########################
 
 # Do not touch this
